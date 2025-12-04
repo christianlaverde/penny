a8b8b091a149f3dc3c9077c5f92dfcd9664e05ea
Christian LaVerde <chrislaverde1@gmail.com>
2025-12-03 20:36:54 -0600

    Move db initialization to __init__.create_app

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ import os
 load_dotenv()
 
 from app.models import db
+from app.utils import setup_test_db
 
 
 def create_app():
@@ -12,15 +13,23 @@ def create_app():
                 static_folder='static',
                 template_folder='templates')
 
-    app.config['APP_ENV'] = os.environ.get('APP_ENV')
+    app.config['APP_ENV'] = os.environ.get('APP_ENV', 'PROD')
+
     if app.config['APP_ENV'] == 'DEV':
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DEV_DATABASE_URI')
     else:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'SECRET-DEV-KEY'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+
+    with app.app_context():
+        if app.config['APP_ENV'] == 'DEV':
+            setup_test_db(app)
+        else:
+            db.create_all()
 
     from app.routes import register_routes
     register_routes(app)
--- run.py
+++ run.py
@@ -1,13 +1,8 @@
-from app import create_app, db
-from app.utils import setup_test_db
+from app import create_app
 
 app = create_app()
 
+
 if __name__ == '__main__':
     with app.app_context():
-        if app.config['APP_ENV'] == 'DEV':
-            setup_test_db(app)
-        else:
-            db.create_all()
-
         app.run(host='0.0.0.0', port=5001, debug=True)
